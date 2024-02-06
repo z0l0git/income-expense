3e4d5f36cb5d792ed57a9e9f3f9536ff6a0f9619
z0l0git <zolbayar.in@gmail.com>
2024-02-06 14:10:52 +0800

    tokenChange change

--- backend-income-expense/middleware/tokenCheck.js
+++ backend-income-expense/middleware/tokenCheck.js
@@ -3,12 +3,12 @@ import jwt from "jsonwebtoken";
 export const tokenCheck = (req, res, next) => {
   //   const { token } = req.headers.authorization;
   const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImlhdCI6MTcwNzE5MjQxMiwiZXhwIjoxNzA3Mjc4ODEyfQ.9oe7Cb4rNzuGU_J8ezHSEgzgYJaZybQeALBJ47FLRzQ";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXIxQGdtYWlsLmNvbSIsImlhdCI6MTcwNzE5MzMzMCwiZXhwIjoxNzA3Mjc5NzMwfQ.c7g4UU7m6KYWSPjYWSyxi2FaIXnOc6dOgvaSw49qkJk";
 
   if (!token) return res.status(401).send("Access denied. No token provided.");
   jwt.verify(token, process.env.JWT_SECRET || "secret", (err, result) => {
     if (err) {
-      res.status(401).send("Access denied. Invalid token.");
+      res.status(401).send(err.message);
       return;
     } else {
       next();
